0eac315f07f7c4194f5622b554e5b995033ad18f
wonde-seni <seni.wonde@gmail.com>
2022-04-15 19:43:58 +0300

    Task 12 Modified Commit Message

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "main.hii"
 /**
  * print_times_table - prints the times table
  * @n: integer for which the times table will be printed
  * Description: prints the times table
  * Return: void
  */
-void times_table(int n)
+void print_times_table(int n)
 {
 	int row, column, product;
 
