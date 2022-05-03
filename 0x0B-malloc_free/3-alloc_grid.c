6d63b77663f91e6214e9c2f3d77a47900c6f4828
wonde-seni <wonde-seni>
2022-05-03 20:42:49 +0300

    Task three Commit Message

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,19 +1,48 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  * free_grid - frees a 2D grid created in alloc_grid.
-  * @grid: pointer to pointer.
-  * @height: height of grid.
-  *
-  * Return: void.
-  */
-void free_grid(int **grid, int height)
+ * alloc_grid - create a matrix using malloc
+ * @width: width of the matrix
+ * @height: height of the matrix
+ *
+ * Return: return a pointer of pointer or null
+ */
+int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, j, a, b;
+	int **p;
 
-	for (i = 0; i < height; i++)
+	if (width <= 0 || height <= 0)
 	{
-		free(grid[i]);
+		return (NULL);
 	}
-	free(grid);
+	else
+	{
+		p = (int **) malloc(height * sizeof(int *));
+		/* we have to make a malloc per pointer*/
+		if (!p)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (i = 0; i < height; i++)
+		{
+			p[i] = (int *) malloc(width * sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
+		}
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				p[a][b] = 0;
+			}
+		}
+		return (p);
+	}
 }
